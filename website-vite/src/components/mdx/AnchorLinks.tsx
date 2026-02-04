26a4f9acc597124ef79cca2cf7b2e788f51703da
Ariella Gilmore <ariellalgilmore@gmail.com>
2026-02-03 17:27:11 -0800

    Delete website-vite directory

--- website-vite/src/components/mdx/AnchorLinks.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AnchorLinks({ children }: { children: React.ReactNode }) {
-  return (
-    <nav className="anchor-links-container" aria-label="Page sections">
-      <ul className="anchor-links-list">{children}</ul>
-    </nav>
-  );
-}
--- website-vite/src/components/mdx/PageDescription.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function PageDescription({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="page-description">
-      {children}
-    </div>
-  );
-}
